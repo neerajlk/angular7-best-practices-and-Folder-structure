16cc5ce62e9412d1e85c4105c8112eac8e2cbfea
Neeraj <neeraj.kamat@transerve.com>
2019-05-03 16:21:38 +0530

    shared and core modules

--- src/app/modules/app.component.ts
+++ src/app/modules/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() { }
 
-  
+  appLoader = true
 }
 
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './components/loader/loader.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [LoaderComponent, HeaderComponent, FooterComponent],
+  declarations: [LoaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
   ],
-  exports: [CommonModule, LoaderComponent, HeaderComponent, FooterComponent]
+  exports: [CommonModule, LoaderComponent]
 })
 export class SharedModule { }
